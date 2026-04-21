fd11424190ff89efd47db59591983d1ebb2d43a0
Grilotrix <89538874+Grilotrix@users.noreply.github.com>
2026-04-20 22:53:15 -0400

    Revert "Require only link and time"

--- src/main/java/p466/team2/scheduleservice/domain/VideoService.java
+++ src/main/java/p466/team2/scheduleservice/domain/VideoService.java
@@ -2,14 +2,6 @@ package p466.team2.scheduleservice.domain;
 
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-
-import static p466.team2.scheduleservice.domain.Utility.getDurationFromLink;
-import static p466.team2.scheduleservice.domain.Utility.getTitleFromLink;
-
 @Service
 public class VideoService {
     private final VideoRepository videoRepository;
@@ -26,19 +18,7 @@ public class VideoService {
         return videoRepository.findById(id).orElseThrow(() -> new VideoNotFoundException(id));
     }
 
-    public Video addVideoToSchedule(Video video) throws IOException {
-        // I moved all this logic here from my Video constructor as I'd rather it takes a second to load when adding a
-        // video versus the more common action of loading the schedule.
-        video.setTitle(getTitleFromLink(video.getLink()));
-        video.setDate(video.getStart().substring(0, video.getStart().indexOf("-")));
-        String rawDuration = getDurationFromLink(video.getLink());
-        String noDecimalDuration = rawDuration.substring(0, rawDuration.indexOf("."));
-        long duration = Long.parseLong(noDecimalDuration) + 1; // Rounding up
-        video.setDuration(duration + "");
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy-HH:mm:ss");
-        video.setStartDateTime(LocalDateTime.parse(video.getStart(), formatter));
-        video.setEndDateTime(video.getStartDateTime().plus(Duration.ofSeconds(duration)));
-
+    public Video addVideoToSchedule(Video video) {
         boolean overlapping = false;
         for (Video other : videoRepository.findAll()) {
             if (video.getStartDateTime().isBefore(other.getEndDateTime()) && other.getStartDateTime().isBefore(video.getEndDateTime())) {
@@ -49,7 +29,6 @@ public class VideoService {
         if (overlapping) {
             throw new VideoOverlapsWithAnotherException(video.getTitle());
         }
-
         return videoRepository.save(video);
     }
 
@@ -59,25 +38,17 @@ public class VideoService {
 
     public Video editVideoDetails(Long id, Video video) {
         return videoRepository.findById(id).map(existingVideo -> {
-            Video videoToUpdate = null;
-            try {
-                videoToUpdate = new Video(
-                                video.getStart(),
-                                video.getLink());
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            videoToUpdate.setId(existingVideo.getId());
-            videoToUpdate.setVersion(existingVideo.getVersion());
-            videoToUpdate.setCreatedDate(existingVideo.getCreatedDate());
-            videoToUpdate.setLastModifiedDate(existingVideo.getLastModifiedDate());
+            var videoToUpdate = new Video(
+                            video.getTitle(),
+                            video.getDate(),
+                            video.getStart(),
+                            video.getDuration(),
+                            video.getLink());
+                            videoToUpdate.setId(existingVideo.getId());
+                            videoToUpdate.setVersion(existingVideo.getVersion());
+                            videoToUpdate.setCreatedDate(existingVideo.getCreatedDate());
+                            videoToUpdate.setLastModifiedDate(existingVideo.getLastModifiedDate());
             return videoRepository.save(videoToUpdate);
-                }).orElseGet(() -> {
-            try {
-                return addVideoToSchedule(video);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
+                }).orElseGet(() -> addVideoToSchedule(video));
     }
 }
